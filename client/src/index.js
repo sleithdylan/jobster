c27b5b73c30fb348d87f1b7595c27d0f94634341
Dylan Sleith <sleithdylan@gmail.com>
2022-12-04 21:28:22 +0000

    style: add dashboard sample

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '@fontsource/public-sans';
+import { StyledEngineProvider } from '@mui/joy/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
