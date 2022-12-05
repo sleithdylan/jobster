3a0093bb433d17aa26115e57e868f3339751a0c9
Dylan Sleith <sleithdylan@gmail.com>
2022-12-05 19:22:54 +0000

    feat: setup routes

--- server.js
+++ server.js
@@ -5,15 +5,27 @@ const port = process.env.PORT || 5000;
 import dotenv from 'dotenv';
 dotenv.config();
 
+// DB and authenticateUser
 import connectDB from './db/connect.js';
+
+// routers
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
 
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+// Make json data available in controllers
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
