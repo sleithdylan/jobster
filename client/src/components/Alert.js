bba0337a4e9e6138a8669aedb89a7210ef0530be
Dylan Sleith <sleithdylan@gmail.com>
2022-12-05 15:42:07 +0000

    refactor: merge register page into login

---
+++ client/src/components/Alert.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Alert from '@mui/joy/Alert';
+
+function Notification() {
+  return <Alert variant="soft">This is an Alert using the soft variant.</Alert>;
+}
+
+export default Notification;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/joy/Link';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Typography from '@mui/joy/Typography';
@@ -9,12 +8,35 @@ import TextField from '@mui/joy/TextField';
 import Checkbox from '@mui/joy/Checkbox';
 import Sheet from '@mui/joy/Sheet';
 import { Grid } from '@mui/material';
+import Notification from '../components/Alert';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+  showAlert: true,
+};
 
 function Login() {
+  const [values, setValues] = useState(initialState);
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <>
       <CssBaseline />
-
       <Grid
         flexDirection="column"
         justifyContent="center"
@@ -31,7 +53,7 @@ function Login() {
             paddingBottom: '1.5rem',
           }}
         >
-          <img src="/jobster-logo.svg" width={150} height={35} alt="" />
+          <img src="/jobster-logo.svg" width={200} height={50} alt="" />
         </Link>
         <Sheet
           component="li"
@@ -43,45 +65,82 @@ function Login() {
           }}
         >
           <Grid flexDirection="column" minWidth={350}>
-            <Typography level="h4" fontWeight="xl">
-              Sign in to your account
-            </Typography>
-            <TextField
-              label="Your email"
-              placeholder="name@company.com"
-              variant="outlined"
+            <Typography
+              level="h4"
+              fontWeight="xl"
               sx={{
-                margin: '1.5rem 0',
-              }}
-            />
-            <TextField
-              type="password"
-              label="Password"
-              placeholder="********"
-              variant="outlined"
-            />
-            <Checkbox
-              size="sm"
-              label="Remember me"
-              sx={{
-                margin: '1.5rem 0',
-              }}
-            />
-            <Button
-              variant="solid"
-              fullWidth
-              sx={{
-                marginBottom: '1.5rem',
+                paddingBottom: '1.5rem',
               }}
             >
-              Sign in
-            </Button>
-            <Typography level="body2">
-              Don’t have an account yet?{' '}
-              <Link component={RouterLink} to="/register" fontWeight="lg">
-                Sign up
-              </Link>
+              {values.isMember
+                ? 'Sign in to your account'
+                : ' Create an account'}
             </Typography>
+            {values.showAlert && <Notification />}
+            <form onSubmit={onSubmit}>
+              {!values.isMember && (
+                <TextField
+                  type="text"
+                  label="Your name"
+                  name="name"
+                  placeholder="e.g. Bonnie Green"
+                  variant="outlined"
+                  sx={{
+                    margin: '1.5rem 0',
+                  }}
+                />
+              )}
+              <TextField
+                type="text"
+                label="Your email"
+                name="email"
+                placeholder="name@company.com"
+                variant="outlined"
+                onChange={handleChange}
+                sx={{
+                  margin: '1.5rem 0',
+                }}
+              />
+              <TextField
+                type="password"
+                label="Password"
+                name="password"
+                placeholder="********"
+                variant="outlined"
+                onChange={handleChange}
+              />
+              <Checkbox
+                size="sm"
+                label="Remember me"
+                sx={{
+                  margin: '1.5rem 0',
+                }}
+              />
+              <Button
+                variant="solid"
+                fullWidth
+                sx={{
+                  marginBottom: '1.5rem',
+                }}
+              >
+                Sign in
+              </Button>
+            </form>
+            {values.isMember ? (
+              <Typography level="body2">
+                Don’t have an account yet?{' '}
+                <Link onClick={toggleMember} fontWeight="lg">
+                  Sign up
+                </Link>
+              </Typography>
+            ) : (
+              <Typography level="body2">
+                Already have an account?{' '}
+                <Link onClick={toggleMember} fontWeight="lg">
+                  Login here
+                </Link>
+              </Typography>
+            )}
           </Grid>
         </Sheet>
       </Grid>
