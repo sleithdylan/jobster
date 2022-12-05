8892536c0a3f94e63822090eaa0896220a49192f
Dylan Sleith <sleithdylan@gmail.com>
2022-12-05 18:41:14 +0000

    feat: connect to database

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 5000;
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import connectDB from './db/connect.js';
+
 // Middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
@@ -13,6 +18,15 @@ app.get('/', (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
