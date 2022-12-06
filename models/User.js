bf7feafb7ee1a642303e75d2290f7883e4ddaab2
Dylan Sleith <sleithdylan@gmail.com>
2022-12-06 16:32:39 +0000

    feat: setup JWT

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import validator from 'validator';
 const { Schema } = mongoose;
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   name: {
@@ -24,6 +25,7 @@ const UserSchema = new Schema({
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -43,5 +45,11 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model('User', UserSchema);
