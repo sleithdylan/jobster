5401000ebcce2d926ea37e9bcb4f8d59d0b241b0
Dylan Sleith <sleithdylan@gmail.com>
2022-12-22 17:07:27 +0000

    feat: added logic and styled addJob page

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -13,6 +13,11 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
+  CLEAR_VALUES,
+  CREATE_JOB_BEGIN,
+  CREATE_JOB_SUCCESS,
+  CREATE_JOB_ERROR,
   LOGOUT_USER,
 } from './actions';
 
@@ -28,7 +33,15 @@ const initialState = {
   user: user ? JSON.parse(user) : null,
   token: token,
   userLocation: userLocation || '',
+  isEditing: false,
+  editJobId: '',
+  position: '',
+  company: '',
   jobLocation: userLocation || '',
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'hybrid', 'internship'],
+  jobType: 'full-time',
+  statusOptions: ['interview', 'declined', 'pending'],
+  status: 'pending',
 };
 
 const AppContext = React.createContext();
@@ -163,6 +176,45 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const handleChange = ({ name, value }) => {
+    dispatch({ type: HANDLE_CHANGE, payload: { name, value } });
+  };
+
+  const clearValues = () => {
+    dispatch({ type: CLEAR_VALUES });
+  };
+
+  const createJob = async () => {
+    dispatch({ type: CREATE_JOB_BEGIN });
+
+    try {
+      const { position, company, jobLocation, jobType, status } = state;
+
+      await authFetch.post('/jobs', {
+        position,
+        company,
+        jobLocation,
+        jobType,
+        status,
+      });
+
+      dispatch({
+        type: CREATE_JOB_SUCCESS,
+      });
+      dispatch({
+        type: CLEAR_VALUES,
+      });
+    } catch (error) {
+      if (error.response.status === 401) return;
+
+      dispatch({
+        type: CREATE_JOB_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -172,6 +224,9 @@ const AppProvider = ({ children }) => {
         loginUser,
         logoutUser,
         updateUser,
+        handleChange,
+        clearValues,
+        createJob,
       }}
     >
       {children}
--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,7 +1,343 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Sidebar from '../../components/Sidebar';
+import Layout from '../../components/Layout';
+import { useAppContext } from '../../context/appContext';
+import Alert from '../../components/Alert';
+import { Link } from 'react-router-dom';
+
+import {
+  IconButton,
+  Box,
+  Typography,
+  Avatar,
+  Menu,
+  MenuItem,
+  Sheet,
+  TextField,
+  Button,
+  Select,
+  Option,
+  selectClasses,
+  FormLabel,
+} from '@mui/joy';
+import { Grid } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MenuIcon from '@mui/icons-material/Menu';
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 
 function AddJob() {
-  return <div>Add Job Page</div>;
+  const {
+    logoutUser,
+    user,
+    displayAlert,
+    showAlert,
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    handleChange,
+    clearValues,
+    createJob,
+  } = useAppContext();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    handleChange({ name, value });
+  };
+
+  const handleJobStatus = (e) => {
+    const value = e.target.textContent;
+
+    handleChange({ name: 'status', value });
+  };
+
+  const handleJobType = (e) => {
+    const value = e.target.textContent;
+
+    handleChange({ name: 'jobType', value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+
+    if (isEditing) {
+      return;
+    }
+
+    createJob();
+  };
+
+  return (
+    <>
+      {drawerOpen && (
+        <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+          <Sidebar />
+        </Layout.SideDrawer>
+      )}
+      <Layout.Root
+        sx={{
+          ...(drawerOpen && {
+            height: '100vh',
+            overflow: 'hidden',
+          }),
+        }}
+      >
+        <Layout.Header>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 1.5,
+            }}
+          >
+            <IconButton
+              variant="outlined"
+              size="sm"
+              onClick={() => setDrawerOpen(true)}
+              sx={{ display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Link
+              component="a"
+              sx={{
+                paddingBottom: '1.5rem',
+              }}
+            >
+              <img src="/jobster-logo.svg" width={125} height={50} alt="" />
+            </Link>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 1.5,
+            }}
+          >
+            <IconButton
+              size="sm"
+              color="primary"
+              sx={{
+                '--IconButton-size': '24px',
+                padding: '0.25rem 0.75rem',
+                background: 'none',
+                '&:hover': {
+                  background: 'none',
+                },
+              }}
+              onClick={handleClick}
+            >
+              <Avatar alt="Remy Sharp" size="md" />
+              <Typography
+                sx={{ marginLeft: '0.75rem', marginRight: '0.25rem' }}
+              >
+                {user?.name}
+              </Typography>
+              <KeyboardArrowDownIcon fontSize="small" color="primary" />
+            </IconButton>
+            <Menu
+              id="menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <Link style={{ textDecoration: 'none' }}>
+                <MenuItem>Profile</MenuItem>
+              </Link>
+              <MenuItem onClick={logoutUser}>Logout</MenuItem>
+            </Menu>
+          </Box>
+        </Layout.Header>
+        <Layout.SideNav>
+          <Sidebar />
+        </Layout.SideNav>
+        <Layout.Main style={{ backgroundColor: '#F7F7F8' }}>
+          <form action="post" onSubmit={handleSubmit}>
+            {showAlert && (
+              <Box sx={{ mb: 2 }}>
+                <Alert />
+              </Box>
+            )}
+            <Sheet
+              component="div"
+              variant="outlined"
+              sx={{
+                borderRadius: 'sm',
+                p: 2,
+                listStyle: 'none',
+              }}
+            >
+              <Grid container columns={12}>
+                <Grid item xs={12} md={4}>
+                  <Typography level="h6">
+                    {isEditing ? 'Edit Job' : 'Add Job'}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Box sx={{ padding: '0 1rem 1rem 1rem' }}>
+                    <TextField
+                      label="Position"
+                      placeholder="e.g. Front-end Engineer"
+                      variant="outlined"
+                      name="position"
+                      value={position}
+                      onChange={handleJobInput}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Box sx={{ padding: '0 1rem 1rem 1rem' }}>
+                    <TextField
+                      label="Company"
+                      placeholder="e.g. Google"
+                      variant="outlined"
+                      name="company"
+                      value={company}
+                      onChange={handleJobInput}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={4}></Grid>
+                <Grid item xs={12} md={5}>
+                  <Box sx={{ padding: '0 1rem 1rem 1rem' }}>
+                    <TextField
+                      label="Job Location"
+                      placeholder="e.g. Dublin"
+                      variant="outlined"
+                      name="jobLocation"
+                      value={jobLocation}
+                      onChange={handleJobInput}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={3}></Grid>
+                <Grid item xs={12} md={4}></Grid>
+                <Grid item xs={12} md={4}>
+                  <Box sx={{ padding: '0 1rem 1rem 1rem' }}>
+                    <FormLabel htmlFor="status" sx={{ pb: '0.25rem' }}>
+                      Status
+                    </FormLabel>
+                    <Select
+                      placeholder="Select a status"
+                      indicator={<KeyboardArrowDown />}
+                      name="status"
+                      value={status}
+                      sx={{
+                        [`& .${selectClasses.indicator}`]: {
+                          transition: '0.2s',
+                          [`&.${selectClasses.expanded}`]: {
+                            transform: 'rotate(-180deg)',
+                          },
+                        },
+                      }}
+                      onChange={handleJobStatus}
+                    >
+                      {statusOptions.map((itemValue, index) => {
+                        return (
+                          <Option key={index} value={itemValue}>
+                            {itemValue}
+                          </Option>
+                        );
+                      })}
+                    </Select>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Box sx={{ padding: '0 1rem 1rem 1rem' }}>
+                    <FormLabel htmlFor="jobType" sx={{ pb: '0.25rem' }}>
+                      Job Type
+                    </FormLabel>
+                    <Select
+                      placeholder="Select a job type"
+                      indicator={<KeyboardArrowDown />}
+                      name="jobType"
+                      value={jobType}
+                      sx={{
+                        [`& .${selectClasses.indicator}`]: {
+                          transition: '0.2s',
+                          [`&.${selectClasses.expanded}`]: {
+                            transform: 'rotate(-180deg)',
+                          },
+                        },
+                      }}
+                      onChange={handleJobType}
+                    >
+                      {jobTypeOptions.map((itemValue, index) => {
+                        return (
+                          <Option key={index} value={itemValue}>
+                            {itemValue}
+                          </Option>
+                        );
+                      })}
+                    </Select>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Sheet>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button
+                variant="outlined"
+                color="neutral"
+                sx={{
+                  px: 4,
+                  mt: 3,
+                  mr: 2,
+                  backgroundColor: '#fff',
+                  '&:hover': {
+                    backgroundColor: '#F7F7F8',
+                    borderColor: '#d8d8df',
+                  },
+                  '&:active': {
+                    backgroundColor: '#EBEBEF',
+                    borderColor: '#d8d8df',
+                  },
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  clearValues();
+                }}
+              >
+                Clear
+              </Button>
+              <Button
+                variant="solid"
+                sx={{ px: 4, mt: 3 }}
+                type="submit"
+                disabled={isLoading}
+              >
+                {isLoading ? 'Please wait...' : 'Save'}
+              </Button>
+            </Box>
+          </form>
+        </Layout.Main>
+      </Layout.Root>
+    </>
+  );
 }
 
 export default AddJob;
