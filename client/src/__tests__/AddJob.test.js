e8df3fee567580ed22dbe71d6da48555620c99be
Dylan Sleith <sleithdylan@gmail.com>
2023-05-24 21:44:38 +0100

    test: add test

---
+++ client/src/__tests__/AddJob.test.js
@@ -0,0 +1,34 @@
+import { BrowserRouter } from 'react-router-dom';
+import { screen, render, fireEvent } from '@testing-library/react';
+import { AppProvider } from 'context/appContext';
+import AddJob from 'pages/dashboard/AddJob';
+
+describe('jobs', () => {
+  test('should insert values into the fields', () => {
+    // Arrange
+    render(
+      <BrowserRouter>
+        <AppProvider>
+          <AddJob />
+        </AppProvider>
+      </BrowserRouter>
+    );
+
+    const positionInput = screen.getByLabelText('Position');
+    const companyInput = screen.getByLabelText('Company');
+    const jobLocationInput = screen.getByLabelText('Job Location');
+    const statusInput = screen.getByDisplayValue('pending');
+    const jobTypeInput = screen.getByDisplayValue('full-time');
+
+    // Act
+    fireEvent.change(positionInput, { target: { value: 'Software Engineer' } });
+    fireEvent.change(companyInput, { target: { value: 'Google' } });
+    fireEvent.change(jobLocationInput, { target: { value: 'Dublin' } });
+    fireEvent.change(statusInput, { target: { value: 'interview' } });
+    fireEvent.change(jobTypeInput, { target: { value: 'remote' } });
+
+    // Assert
+    expect(positionInput).toHaveValue('Software Engineer');
+    expect(statusInput).toHaveValue('interview');
+  });
+});
